033161d0a4caea048dde9004ebcb2bf3d61ab901
AshwiniNishaKrishnan <99712110+AshwiniNishaKrishnan@users.noreply.github.com>
2022-08-08 11:57:33 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ host : '172.31.30.115',
 ref : 'main',
 repo : 'git@github.com:AshwiniNishaKrishnan/ecosystem.git',
 path : '/home/nisha',
-key  : '/home/nisha/.ssh/id_rsa.pub',
+key  : '/home/nisha/.ssh/id_rsa',
 'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
 }
 }
